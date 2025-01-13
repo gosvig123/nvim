7c4a220a3e21e233912c865a323e679eca7fc01a
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2025-01-13 17:23:20 +0100

    updated clip board key mapping

--- lua/plugins/lazyclip.lua
+++ lua/plugins/lazyclip.lua
@@ -1,14 +1,15 @@
 return {
-        "atiladefreitas/lazyclip",
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-        },
-        config = function()
-            require("lazyclip").setup()
-        end,
-        keys = {
-            { "<leader>Cw", "<cmd>LazyClip<cr>", desc = "Open Clipboard Manager" },
-        },
-        -- Optional: Load plugin when yanking text
-        event = { "TextYankPost" },
+  "atiladefreitas/lazyclip",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  config = function()
+    require("lazyclip").setup()
+  end,
+  keys = {
+    { "<leader>oc", "<cmd>LazyClip<cr>", desc = "Open Clipboard Manager" },
+  },
+  -- Optional: Load plugin when yanking text
+  event = { "TextYankPost" },
 }
+
