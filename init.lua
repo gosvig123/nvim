c20f08d81a672f3b7b374822b1815751735ba942
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2025-01-26 15:36:26 +0100

    changed to use avante config and added env system

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
+local env = require("env")
+vim.env.DEEPSEEK_API_KEY = env.DEEPSEEK_API_KEY
+-- display all secrets when starting neovim
+-- vim.cmd("set shortmess+=I")
+
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
