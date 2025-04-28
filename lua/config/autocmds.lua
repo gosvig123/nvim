070a22b0a993c0f1959c300336e802b75a29522b
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2025-04-28 17:33:54 +0200

    augment and auto cmd updated

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,11 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  pattern = "*",
+  callback = function()
+    if vim.bo.modified and vim.bo.modifiable and not vim.bo.readonly then
+      pcall(vim.cmd, "silent write")
+    end
+  end,
+})
--- lua/plugins/augment.lua
+++ lua/plugins/augment.lua
@@ -1,3 +1,19 @@
 return {
-  { "augmentcode/augment.vim" },
+  {
+    "augmentcode/augment.vim",
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      -- Set workspace folders before plugin loads
+      vim.g.augment_workspace_folders = {
+        vim.fn.expand("~/.config/nvim"), -- Your config
+        vim.fn.getcwd(), -- Current project
+      }
+      -- Optional customizations:
+      vim.g.augment_disable_tab_mapping = false -- Keep default tab completion
+      vim.g.augment_disable_completions = false -- Force enable
+    end,
+  },
 }
