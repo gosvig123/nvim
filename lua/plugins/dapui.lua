79c8f5955d14a2cc803139c14b961331b0a4ecb2
gosvig123 <109414278+gosvig123@users.noreply.github.com>
2025-07-07 06:47:56 +0200

    remove litee and some extras

---
+++ lua/plugins/dapui.lua
@@ -0,0 +1,79 @@
+return {
+  "rcarriga/nvim-dap-ui",
+  dependencies = { "mfussenegger/nvim-dap" },
+  config = function()
+    local dapui = require("dapui")
+    dapui.setup({
+      icons = { expanded = "▾", collapsed = "▸" },
+      mappings = {
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        edit = "e",
+        repl = "r",
+        toggle = "t",
+      },
+      element_mappings = {},
+      expand_lines = true,
+      force_buffers = true,
+      floating = {
+        max_height = nil,
+        max_width = nil,
+        border = "single",
+        mappings = {
+          close = { "q", "<Esc>" },
+        },
+      },
+      controls = {
+        enabled = true,
+        element = "repl",
+        icons = {
+          pause = "",
+          play = "",
+          step_into = "",
+          step_over = "",
+          step_out = "",
+          step_back = "",
+          run_last = "↻",
+          terminate = "□",
+        },
+      },
+      layouts = {
+        {
+          elements = {
+            { id = "scopes", size = 0.25 },
+            { id = "breakpoints", size = 0.25 },
+            { id = "stacks", size = 0.25 },
+            { id = "watches", size = 0.25 },
+          },
+          size = 40,
+          position = "left",
+        },
+        {
+          elements = {
+            { id = "repl", size = 0.5 },
+            { id = "console", size = 0.5 },
+          },
+          size = 10,
+          position = "bottom",
+        },
+      },
+      render = {
+        virtual_text = true,
+        colored_value = true,
+        indent = 1,
+      },
+    })
+
+    local dap = require("dap")
+    dap.listeners.after.event_initialized["dapui_config"] = function()
+      dapui.open()
+    end
+    dap.listeners.before.event_terminated["dapui_config"] = function()
+      dapui.close()
+    end
+    dap.listeners.before.event_exited["dapui_config"] = function()
+      dapui.close()
+    end
+  end,
+}
